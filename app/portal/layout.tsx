c14646bbaffe8a8f1a8c55f780a747b42273b534
D3mzy-108 <demil.ayinde@gmail.com>
2025-07-06 05:50:45 -0700

    Slight UI modification to prevent overflow on mobile browser

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -13,9 +13,9 @@ export default function PortalLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full min-h-screen relative">
-      <div className="fixed top-3 left-3 w-[300px] aspect-square bg-black/80 animated-backdrop-container"></div>
-      <div className="z-10 bg-gradient-to-b from-white/90 to-white/40 to-40% w-screen min-h-screen backdrop-blur-2xl">
+    <div className="w-full min-height-screen relative">
+      <div className="fixed top-3 left-3 w-[300px] aspect-square bg-black/40 md:bg-black/80 animated-backdrop-container"></div>
+      <div className="z-10 bg-gradient-to-b from-white/90 to-white/40 to-40% w-screen min-height-screen backdrop-blur-2xl">
         <LayoutChild>{children}</LayoutChild>
       </div>
     </div>
